6bca9a8b4661a67e7e8812e6147908564182e83e
Otacílio Beleza <otacilio.system@gmail.com>
2022-05-11 07:19:38 -0400

    Inicio das validações

--- visa/views.py
+++ visa/views.py
@@ -26,12 +26,20 @@ class CnpjView(TemplateView):
     template_name = 'cnpj.html'
 
     def get_context_data(self, **kwargs):
+        ''' varivarel para testes
         cod = ['47.44-0-99', '45.30-7-03', '47.41-5-00', '47.42-3-00',
                '47.43-1-00', '47.44-0-01', '47.53-9-00', '47.54-7-01',
                '47.54-7-03', '49.30-2-01', '49.30-2-02', '77.19-5-99',
-               '77.32-2-01', '43.19-3-00', '43.11-8-02']
+               '77.32-2-01', '43.19-3-00', '43.11-8-02']'''
+
         context = super(CnpjView, self).get_context_data(**kwargs)
         search = self.request.GET.get('search')
-        buscar = pega_resultado(search)
-        context['atividades'] = Atividades.objects.filter(cnae__in=buscar)
+        context['atividades'] = ''
+        if search:
+            cnaelist = pega_resultado(search)
+            context['atividades'] = Atividades.objects.filter(cnae__in=cnaelist)
+
+            if type(cnaelist) == str:
+                context['atividades'] = "vazio"
+
         return context
